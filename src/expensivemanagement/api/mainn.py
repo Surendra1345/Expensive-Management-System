c14ef25b08a9547ff3fcde7f4462e20f887422df
Kamireddy Surendranath Reddy <surendrareddy49175@gmail.com>
2026-07-21 13:24:28 +0530

    Delete src/expensivemanagement directory

--- src/expensivemanagement/api/mainn.py
+++
@@ -1,8 +0,0 @@
-from api.Authentication.database import engine, Base
-
-
-import Authentication.models as models
-
-Base.metadata.create_all(bind=engine)
-
-print("Tables created successfully!")
--- src/expensivemanagement/expense-reimbursement/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from expense-reimbursement!")
-
-
-if __name__ == "__main__":
-    main()
